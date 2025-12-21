a2dc79a8ddad149804ad3cd2e67707ba633fb6a0
tongshuanglong <tonngshuanglong@pugongying.com.cn>
2025-12-22 01:02:08 +0800

    feat(docs): 配置SpringDoc以启用API文档分组和自定义设置
    - 设置swagger-ui标签排序为字母顺序
    - 禁用通用响应覆盖功能
    - 启用API文档分组功能

--- releasehub-bootstrap/src/main/java/io/releasehub/bootstrap/ReleaseHubApplication.java
+++ releasehub-bootstrap/src/main/java/io/releasehub/bootstrap/ReleaseHubApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReleaseHubApplication {
 
     public static void main(String[] args) {
+        System.setProperty("springdoc.swagger-ui.tagsSorter", "alpha");
+        System.setProperty("springdoc.override-with-generic-response", "false");
+        System.setProperty("springdoc.api-docs.groups.enabled", "true");
         SpringApplication.run(ReleaseHubApplication.class, args);
     }
 }
