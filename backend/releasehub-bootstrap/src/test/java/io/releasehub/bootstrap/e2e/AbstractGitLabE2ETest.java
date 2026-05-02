48dbd8ae285da8df64fab0873090e39929ae0475
tongshuanglong <tongsh6@gmail.com>
2026-05-02 21:42:51 +0800

    fix: add missing MediaType import after wildcard replacement

--- backend/releasehub-bootstrap/src/test/java/io/releasehub/bootstrap/e2e/AbstractGitLabE2ETest.java
+++ backend/releasehub-bootstrap/src/test/java/io/releasehub/bootstrap/e2e/AbstractGitLabE2ETest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
